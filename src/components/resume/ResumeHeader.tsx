3fafc8a676b0622909deb839df2a0756857c2957
Sheehan, Ryan <ryan.sheehan@tylertech.com>
2019-05-28 00:35:42 -0500

    added downloaded version

--- src/components/resume/ResumeHeader.tsx
+++ src/components/resume/ResumeHeader.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Typography, Theme } from '@material-ui/core';
+import { Typography, Theme, Icon, Fab } from '@material-ui/core';
 
 const useStyle = makeStyles((theme: Theme) => ({
   root: {
     padding: `${theme.spacing(2)}px`,
-    backgroundColor: theme.palette.primary.dark
+    backgroundColor: theme.palette.primary.dark,
+    display: 'grid',
+    gridTemplateColumns: '1fr auto',
+    gridTemplateRows: '1fr 1fr',
+    gridTemplateAreas: `
+      "title icon"
+      "subtitle icon"
+    `,
+    alignItems: 'center'
+  },
+  download: {
+    gridArea: 'icon'
+  },
+  mainTitle: {
+    gridArea: 'title'
+  },
+  subtitle: {
+    gridArea: 'subtitle'
   }
 }))
 
 export const ResumeHeader: React.FC<{name: string, title: string}> =
 ({name, title}) => {
-  const {root} = useStyle();
+  const {root, mainTitle, download, subtitle} = useStyle();
 
   return (
     <div className={root}>
-      <Typography variant="h2" color="textSecondary">{name}</Typography>
-      <Typography variant="subtitle1" color="textSecondary">{title}</Typography>
+      <Typography className={mainTitle} variant="h2" color="textSecondary">{name}</Typography>
+      <Typography className={subtitle} variant="subtitle1" color="textSecondary">{title}</Typography>
+      <Fab className={download} href="resume.pdf" target="_blank">
+        <Icon>save_alt</Icon>
+      </Fab>
     </div>
   );
 }
